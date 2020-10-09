babf3c5a474a4f5ecc064fe33b432d467d0087c9
uzerp2 <uzerp2apple@gmail.com>
2020-10-09 16:49:08 +0300

    8/9 redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {connect} from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
+
 
 const Header = ({ currentUser }) => (
   <div className='header'>
@@ -32,4 +34,11 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+
+
+export default connect(mapStateToProps)(Header);
--- src/index.js
+++ src/index.js
@@ -5,13 +5,18 @@ import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <Provider store={store} >
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
   ,
   document.getElementById('root')
 );
